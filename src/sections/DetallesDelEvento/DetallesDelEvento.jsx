fec87f082d3ff37d9eb92fcb276e47e54c04fdc8
Paúl Pérez <paulperezsv@gmail.com>
2026-09-08 22:32:43 -0600

    feat: implement DetallesDelEvento section with event details and navigation links

--- src/sections/DetallesDelEvento/DetallesDelEvento.jsx
+++ src/sections/DetallesDelEvento/DetallesDelEvento.jsx
@@ -17,10 +17,10 @@ function DetallesDelEvento({ zIndex, marginTop, marginBottom }) {
         backgroundImage: `url(${tarjetaImg})`,
     };
 
-	return (
-		<section id="detalles-del-evento" className="section-detalles" style={sectionStyle}>
-			<div className="detalles-tarjeta" style={tarjetaStyle}>
-				<img className="detalles-rosa" src={rosaImg} alt="Rosa azul" />
+    return (
+        <section id="detalles-del-evento" className="section-detalles" style={sectionStyle}>
+            <div className="detalles-tarjeta" style={tarjetaStyle}>
+                <img className="detalles-rosa" src={rosaImg} alt="Rosa azul" />
                 <div className="detalles-titulo">
                     <span>Detalles</span>
                     <span>del evento</span>
@@ -40,15 +40,15 @@ function DetallesDelEvento({ zIndex, marginTop, marginBottom }) {
                 </div>
                 <div className="detalles-botones">
                     {/* {Google Maps} */}
-                    <a href="https://goo.gl/maps/6Z1g5k7Q8y2x3J9F9" target="_blank" rel="noopener noreferrer">Google Maps</a>
+                    <a href="https://www.google.com/maps/place/Cielo+Abierto/@13.6164551,-89.1836676,1026m/data=!3m2!1e3!4b1!4m6!3m5!1s0x8f6333ec15b7be99:0x611887bff381088d!8m2!3d13.6164551!4d-89.1836676!16s%2Fg%2F11byvqtc42?entry=ttu&g_ep=EgoyMDI2MDkwMi4wIKXMDSoASAFQAw%3D%3D" target="_blank" rel="noopener noreferrer">Google Maps</a>
                     {/* {Waze} */}
-                    <a href="https://www.waze.com/" target="_blank" rel="noopener noreferrer">Waze</a>
+                    <a href="https://www.waze.com/es/live-map/directions/cielo-abierto-rn-6-carretera-a-panchimalco?to=place.w.177471624.1774847314.6913731" target="_blank" rel="noopener noreferrer">Waze</a>
                 </div>
-				<img className="detalles-rosa-bl-1" src={rosaBLImg1} alt="Rosa azul" />
-				<img className="detalles-rosa-bl-2" src={rosaBLImg2} alt="Rosa azul" />
-			</div>
-		</section>
-	);
+                <img className="detalles-rosa-bl-1" src={rosaBLImg1} alt="Rosa azul" />
+                <img className="detalles-rosa-bl-2" src={rosaBLImg2} alt="Rosa azul" />
+            </div>
+        </section>
+    );
 }
 
 export default DetallesDelEvento;
